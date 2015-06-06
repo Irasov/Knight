5fb0c185a8d75d75ed0ed5acb519e90a2c842acc
Irasov <srnan@yandex.ru>
2015-06-06 22:11:51 +0600

    List

--- src/main/java/com/epam/irasov/knight/logic/SearchByPrice.java
+++ src/main/java/com/epam/irasov/knight/logic/SearchByPrice.java
@@ -4,15 +4,16 @@ import com.epam.irasov.knight.entity.*;
 import com.epam.irasov.knight.entity.Knight;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class SearchByPrice {
     private Knight knight;
-    private ArrayList<Ammunition> selectedAmmunition = new ArrayList<Ammunition>();
+    private List<Ammunition> selectedAmmunition = new ArrayList<Ammunition>();
     int min;
     int max;
 
-    public ArrayList<Ammunition> searchPrice(Knight knight, int min, int max){
-        ArrayList<Ammunition> ammunitions = knight.getAmmunitionList();
+    public List<Ammunition> searchPrice(Knight knight, int min, int max){
+        List<Ammunition> ammunitions = knight.getAmmunitionList();
         for(Ammunition ammunition: ammunitions){
             if (ammunition.getPrice()>=min && ammunition.getPrice()<=max){
                 selectedAmmunition.add(ammunition);
