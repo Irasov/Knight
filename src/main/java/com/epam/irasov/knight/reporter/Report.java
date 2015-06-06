bee1b0391bf887b251bcfa7153bee3a14172c2a0
Irasov <srnan@yandex.ru>
2015-06-06 21:52:40 +0600

    new report

--- src/main/java/com/epam/irasov/knight/reporter/Report.java
+++ src/main/java/com/epam/irasov/knight/reporter/Report.java
@@ -1,8 +1,11 @@
 package com.epam.irasov.knight.reporter;
 
 
+import com.epam.irasov.knight.entity.Ammunition;
 import com.epam.irasov.knight.entity.Knight;
 import com.epam.irasov.knight.logic.SearchByPrice;
+
+import java.util.ArrayList;
 
 public class Report {
     private SearchByPrice search = new SearchByPrice();
@@ -15,7 +18,12 @@ public class Report {
         System.out.println( "Sum gold: "+knight.calculatePrice());
     }
 
-    public void reportSearch(Knight knight){
-        System.out.println(search.searchPrice(knight));
+    public void reportSearch(Knight knight,int min,int max){
+        ArrayList<Ammunition> selectedAmmunition= search.searchPrice(knight, min, max);
+        String str="Ammunition in the price band reception from "+min+" to "+max+":\n";
+        for(Ammunition ammunition:selectedAmmunition){
+            str+=ammunition.toString()+"\n";
+        }
+        System.out.println(str);
     }
 }
