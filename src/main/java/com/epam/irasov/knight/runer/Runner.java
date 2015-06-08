9eed6d6a835a7e0f05085b9451c64cf44d701d34
Irasov <srnan@yandex.ru>
2015-06-08 23:11:59 +0600

    call static methods

--- src/main/java/com/epam/irasov/knight/runer/Runner.java
+++ src/main/java/com/epam/irasov/knight/runer/Runner.java
@@ -3,7 +3,7 @@ package com.epam.irasov.knight.runer;
 import com.epam.irasov.knight.creator.Creator;
 import com.epam.irasov.knight.entity.Ammunition;
 import com.epam.irasov.knight.entity.Knight;
-import com.epam.irasov.knight.logic.Sort;
+import com.epam.irasov.knight.logic.Action;
 import com.epam.irasov.knight.reporter.Report;
 
 import java.util.Collections;
@@ -11,14 +11,11 @@ import java.util.Comparator;
 
 public class Runner {
     public static void main(String[] args) {
-        Report report = new Report();
         Knight knight = new Knight("Don Quixote");
-        Creator creator = new Creator(knight,7);
-        Sort sort = new Sort();
+        Creator creator = new Creator(knight, 7);
         creator.CreateAmmunition();
-        sort.sortAmmunition(knight,"weight");
-        report.reportKnight(knight);
-        report.reportSumPrice(knight);
-        report.reportSearch(knight,500,1500);
+        Action.sortAmmunition(knight.getAmmunitionList(), "weight");
+        Report.report(knight);
+        Report.report(Action.searchPrice(knight.getAmmunitionList(),500,1500));
     }
 }
