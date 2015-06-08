a920722da82e069a6759db8f1f29c7e53f604c79
Irasov <srnan@yandex.ru>
2015-06-08 23:10:41 +0600

    static report

--- src/main/java/com/epam/irasov/knight/reporter/Report.java
+++ src/main/java/com/epam/irasov/knight/reporter/Report.java
@@ -3,28 +3,23 @@ package com.epam.irasov.knight.reporter;
 
 import com.epam.irasov.knight.entity.Ammunition;
 import com.epam.irasov.knight.entity.Knight;
-import com.epam.irasov.knight.logic.SearchByPrice;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Report {
-    private SearchByPrice search = new SearchByPrice();
 
-    public void reportKnight(Knight knight){
+    public static void report(Knight knight){
         System.out.println(knight.toString());
     }
 
-    public void reportSumPrice(Knight knight){
-        System.out.println( "Sum gold: "+knight.calculatePrice());
+    public static void report(List<Ammunition> a){
+        StringBuilder sb = new StringBuilder();
+        sb.append("ammunition elements from the range: \n");
+        for(Ammunition element:a){
+            sb.append(element.toString()+"\n");
+        }
+        System.out.println(sb);
     }
 
-    public void reportSearch(Knight knight,int min,int max){
-        List<Ammunition> selectedAmmunition= search.searchPrice(knight, min, max);
-        String str="Ammunition in the price band reception from "+min+" to "+max+":\n";
-        for(Ammunition ammunition:selectedAmmunition){
-            str+=ammunition.toString()+"\n";
-        }
-        System.out.println(str);
-    }
 }
