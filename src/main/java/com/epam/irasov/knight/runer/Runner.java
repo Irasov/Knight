9e72569026d5a7691c5ed1ce89b8c95866fc1124
Irasov <srnan@yandex.ru>
2015-06-11 10:13:15 +0600

    add const

--- src/main/java/com/epam/irasov/knight/runer/Runner.java
+++ src/main/java/com/epam/irasov/knight/runer/Runner.java
@@ -10,12 +10,18 @@ import java.util.Collections;
 import java.util.Comparator;
 
 public class Runner {
+    public static final String CUSTOM_NAME_KNIGHT = "Don Quixote";
+    public static final String CUSTOM_SORT_CRITERION = "weight";
+    public static final int CUSTOM_AMOUNT_AMMUNITION = 7;
+    public static final int CUSTOM_MIN_PRICE = 500;
+    public static final int CUSTOM_MAX_PRICE = 1500;
+
     public static void main(String[] args) {
-        Knight knight = new Knight("Don Quixote");
-        Creator creator = new Creator(knight, 7);
+        Knight knight = new Knight(CUSTOM_NAME_KNIGHT);
+        Creator creator = new Creator(knight, CUSTOM_AMOUNT_AMMUNITION);
         creator.CreateAmmunition();
-        Action.sortAmmunition(knight.getAmmunitionList(), "weight");
+        Action.sortAmmunition(knight.getAmmunitionList(), CUSTOM_SORT_CRITERION);
         Report.report(knight);
-        Report.report(Action.searchPrice(knight.getAmmunitionList(),500,1500));
+        Report.report(Action.searchPrice(knight.getAmmunitionList(),CUSTOM_MIN_PRICE,CUSTOM_MAX_PRICE));
     }
 }
