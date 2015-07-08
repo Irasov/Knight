69acedc01fa292ef2b689e3ad87aa76f85496c71
Irasov <srnan@yandex.ru>
2015-07-08 09:46:37 +0600

    switch

--- src/main/java/com/epam/irasov/knight/creator/Creator.java
+++ src/main/java/com/epam/irasov/knight/creator/Creator.java
@@ -1,7 +1,6 @@
 package com.epam.irasov.knight.creator;
 
 
-
 import com.epam.irasov.knight.entity.*;
 
 import java.util.Random;
@@ -24,64 +23,65 @@ public class Creator {
     private Knight knight;
     private int countAmmunition;
 
-    public Creator(Knight knight, int countAmmunition){
+    public Creator(Knight knight, int countAmmunition) {
         this.knight = knight;
-        this.countAmmunition=countAmmunition;
+        this.countAmmunition = countAmmunition;
     }
 
-    public void setCountAmmunition(int countAmmunition){
-        this.countAmmunition=countAmmunition;
+    public void setCountAmmunition(int countAmmunition) {
+        this.countAmmunition = countAmmunition;
     }
 
     public int getCountAmmunition() {
         return countAmmunition;
     }
 
-    private Armor.ArmorType randomArmorType(){
+    private Armor.ArmorType randomArmorType() {
         Armor.ArmorType[] values = Armor.ArmorType.values();
         return values[RANDOM.nextInt(values.length)];
     }
 
-    private Armor.ConstProtection randomConstProtection(){
+    private Armor.ConstProtection randomConstProtection() {
         Armor.ConstProtection[] values = Armor.ConstProtection.values();
         return values[RANDOM.nextInt(values.length)];
     }
 
-    private  Weapon.Captured randomCaptured() {
-        Weapon.Captured [] values =  Weapon.Captured.values();
+    private Weapon.Captured randomCaptured() {
+        Weapon.Captured[] values = Weapon.Captured.values();
         return values[RANDOM.nextInt(values.length)];
     }
 
-    private MelleWeapon.MelleWeaponType randomMelleWeapon(){
+    private MelleWeapon.MelleWeaponType randomMelleWeapon() {
         MelleWeapon.MelleWeaponType[] values = MelleWeapon.MelleWeaponType.values();
         return values[RANDOM.nextInt(values.length)];
     }
 
-    private RangedWeapon.RangedWeaponType randomRangedWeapon(){
+    private RangedWeapon.RangedWeaponType randomRangedWeapon() {
         RangedWeapon.RangedWeaponType[] values = RangedWeapon.RangedWeaponType.values();
         return values[RANDOM.nextInt(values.length)];
     }
 
-    private Shield.ShieldType randomShieldType(){
+    private Shield.ShieldType randomShieldType() {
         Shield.ShieldType[] values = Shield.ShieldType.values();
         return values[RANDOM.nextInt(values.length)];
     }
 
-    private Shield.ShieldMaterial randomShieldMaterial(){
+    private Shield.ShieldMaterial randomShieldMaterial() {
         Shield.ShieldMaterial[] values = Shield.ShieldMaterial.values();
         return values[RANDOM.nextInt(values.length)];
     }
 
-    private Helmet.HelmetType randomHelmetType(){
+    private Helmet.HelmetType randomHelmetType() {
         Helmet.HelmetType[] values = Helmet.HelmetType.values();
         return values[RANDOM.nextInt(values.length)];
     }
 
-    private Helmet.ConstBalaclava randomConstBalaclava (){
+    private Helmet.ConstBalaclava randomConstBalaclava() {
         Helmet.ConstBalaclava[] values = Helmet.ConstBalaclava.values();
         return values[RANDOM.nextInt(values.length)];
     }
-    private Ammunition.ConstAmmunition randomConstAmmunition (){
+
+    private Ammunition.ConstAmmunition randomConstAmmunition() {
         Ammunition.ConstAmmunition[] values = Ammunition.ConstAmmunition.values();
         return values[RANDOM.nextInt(values.length)];
     }
@@ -89,40 +89,41 @@ public class Creator {
 
     public void CreateAmmunition() {
         Ammunition componentAmmunition = null;
-        for(int i=1; i<=this.countAmmunition;i++) {
-            Ammunition.ConstAmmunition randName = randomConstAmmunition ();
-            randName = randomConstAmmunition();
-            if (randName.equals(Ammunition.ConstAmmunition.ARMOR)) {
-                int randWeight = (RANDOM.nextInt(ARMOR_WEIGHT) + ADDITIVE_NOT_RANDOM_ZERO);
-                int randPrice = (RANDOM.nextInt(PRICE_ARMOR) + ADDITIVE_PRICE);
-                componentAmmunition = new Armor(randName, randomArmorType(), randWeight, randPrice,randomConstProtection());
-                knight.addAmmunition(componentAmmunition);
-            }else
-            if (randName.equals(Ammunition.ConstAmmunition.HELMET)) {
-                int randWeight = (RANDOM.nextInt(HELMET_WEIGHT) + ADDITIVE_NOT_RANDOM_ZERO);
-                int randPrice = (RANDOM.nextInt(PRICE_HELMET) + ADDITIVE_PRICE);
-                componentAmmunition = new Helmet(randName, randomHelmetType(), randWeight, randPrice, randomConstBalaclava());
-                knight.addAmmunition(componentAmmunition);
-            }else
-            if (randName.equals(Ammunition.ConstAmmunition.SHIELD)) {
-                int randWeight = (RANDOM.nextInt(SHIELD_WEIGHT) + ADDITIVE_NOT_RANDOM_ZERO);
-                int randPrice = (RANDOM.nextInt(PRICE_SHIELD) + ADDITIVE_PRICE);
-                componentAmmunition = new Shield(randName, randomShieldType(), randWeight, randPrice,randomShieldMaterial());
-                knight.addAmmunition(componentAmmunition);
-            }else
-            if (randName.equals(Ammunition.ConstAmmunition.MELLE_WEAPON)) {
-                int randWeight = (RANDOM.nextInt(WEAPON_WEIGHT) + ADDITIVE_NOT_RANDOM_ZERO);
-                int randPrice = (RANDOM.nextInt(PRICE_WEAPON) + ADDITIVE_PRICE);
-                int randomLengthWeapon = (RANDOM.nextInt(LENGTH_WEAPON) + ADDITIVE_NOT_RANDOM_ZERO);
-                componentAmmunition = new MelleWeapon(randName, randomMelleWeapon(), randWeight, randPrice, randomCaptured(), randomLengthWeapon);
-                knight.addAmmunition(componentAmmunition);
-            }else
-            if (randName.equals(Ammunition.ConstAmmunition.RANGED_WEAPON)) {
-                int randWeight = (RANDOM.nextInt(WEAPON_WEIGHT) + ADDITIVE_NOT_RANDOM_ZERO);
-                int randPrice = (RANDOM.nextInt(PRICE_WEAPON) + ADDITIVE_PRICE);
-                int randomNumberOfShells = (RANDOM.nextInt(SHELLS) + ADDITIVE_NOT_RANDOM_ZERO);
-                componentAmmunition = new RangedWeapon(randName, randomRangedWeapon(), randWeight, randPrice, randomCaptured(), randomNumberOfShells);
-                knight.addAmmunition(componentAmmunition);
+        for (int i = 1; i <= this.countAmmunition; i++) {
+            Ammunition.ConstAmmunition randName = randomConstAmmunition();
+            switch (randName) {
+                case ARMOR:
+                    int randWeight = (RANDOM.nextInt(ARMOR_WEIGHT) + ADDITIVE_NOT_RANDOM_ZERO);
+                    int randPrice = (RANDOM.nextInt(PRICE_ARMOR) + ADDITIVE_PRICE);
+                    componentAmmunition = new Armor(randName, randomArmorType(), randWeight, randPrice, randomConstProtection());
+                    knight.addAmmunition(componentAmmunition);
+                    break;
+                case HELMET:
+                    randWeight = (RANDOM.nextInt(HELMET_WEIGHT) + ADDITIVE_NOT_RANDOM_ZERO);
+                    randPrice = (RANDOM.nextInt(PRICE_HELMET) + ADDITIVE_PRICE);
+                    componentAmmunition = new Helmet(randName, randomHelmetType(), randWeight, randPrice, randomConstBalaclava());
+                    knight.addAmmunition(componentAmmunition);
+                    break;
+                case SHIELD:
+                    randWeight = (RANDOM.nextInt(SHIELD_WEIGHT) + ADDITIVE_NOT_RANDOM_ZERO);
+                    randPrice = (RANDOM.nextInt(PRICE_SHIELD) + ADDITIVE_PRICE);
+                    componentAmmunition = new Shield(randName, randomShieldType(), randWeight, randPrice, randomShieldMaterial());
+                    knight.addAmmunition(componentAmmunition);
+                    break;
+                case MELLE_WEAPON:
+                    randWeight = (RANDOM.nextInt(WEAPON_WEIGHT) + ADDITIVE_NOT_RANDOM_ZERO);
+                    randPrice = (RANDOM.nextInt(PRICE_WEAPON) + ADDITIVE_PRICE);
+                    int randomLengthWeapon = (RANDOM.nextInt(LENGTH_WEAPON) + ADDITIVE_NOT_RANDOM_ZERO);
+                    componentAmmunition = new MelleWeapon(randName, randomMelleWeapon(), randWeight, randPrice, randomCaptured(), randomLengthWeapon);
+                    knight.addAmmunition(componentAmmunition);
+                    break;
+                case RANGED_WEAPON:
+                    randWeight = (RANDOM.nextInt(WEAPON_WEIGHT) + ADDITIVE_NOT_RANDOM_ZERO);
+                    randPrice = (RANDOM.nextInt(PRICE_WEAPON) + ADDITIVE_PRICE);
+                    int randomNumberOfShells = (RANDOM.nextInt(SHELLS) + ADDITIVE_NOT_RANDOM_ZERO);
+                    componentAmmunition = new RangedWeapon(randName, randomRangedWeapon(), randWeight, randPrice, randomCaptured(), randomNumberOfShells);
+                    knight.addAmmunition(componentAmmunition);
+                    break;
             }
         }
     }
